a473fd18a49c4a1410b75b4668ec5d8c3fa3cd3a
Daniel Garcia <danielrichardgarcia2@gmail.com>
2020-11-21 14:27:20 -0700

    added requirements.txt and set up for Heroku deployment

--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config(object):
     ADMINS = os.environ.get('ADMINS')
     POSTS_PER_PAGE = 25
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
